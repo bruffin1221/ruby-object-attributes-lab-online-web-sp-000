4b9d9da47a4192361de3ff5d5ff0759f8fc46b9c
Bruce Ruffin <bruffin1221@gmail.com>
2019-10-07 22:01:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,3 @@
-class Person
-
-  def initialize(name)
-    @name = name
-  end
-
-  def name
-    @name
-  end
-
-  def name=(new_name)
-    @name = new_name
-  end
-
-end
-
-kanye = Person.new("Kanye")
-kanye.name
-
 class Dog
   def initialize(name)
     @name=name
